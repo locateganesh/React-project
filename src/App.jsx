44c3d1529d592f8bcb90e52c543f3bc20f3ef0cd
ganyadav <99638611+ganyadav@users.noreply.github.com>
2025-03-30 07:37:24 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import CartContextProvider from './store/shopping-cart-context.jsx';
+
+function App() {
+
+  return (
+    // <CartContext.Provider value={{items: []}}> // requires a default value
+    <CartContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContextProvider>
+  );
+}
+
+export default App;
