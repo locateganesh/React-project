1ef5c69d57575638e42d06cbfc83b86791dfa6ff
ganyadav <99638611+ganyadav@users.noreply.github.com>
2025-05-16 10:30:18 +0530

    form actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
-import LoginUseRef from './components/LoginUseRef.jsx';
 import Signup from './components/Signup';
 
 function App() {
@@ -8,9 +6,7 @@ function App() {
     <>
       <Header />
       <main>
-        <Login />
-        {/* <LoginUseRef /> */}
-        {/* <Signup /> */}
+        <Signup />
       </main>
     </>
   );
