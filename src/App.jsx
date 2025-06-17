5da089befe56b39b63f768a3231b71ef5ec92a8c
ganyadav <99638611+ganyadav@users.noreply.github.com>
2025-06-17 18:32:12 +0530

    finished App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Header } from './components/Header';
-import { Opinions } from './components/Opinions';
-import { NewOpinion } from './components/NewOpinion';
-import { OpinionsContextProvider } from './store/opinions-context';
-
+import Header from './components/Header/Hader.jsx';
+import Meals from './components/Meals/Meals.jsx';
+import { CartContextProvider } from './store/CartContext.jsx';
+import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
+import Cart from './components/Cart/Cart.jsx';
+import Checkout from './components/Checkout/Checkout.jsx';
 function App() {
   return (
-    <>
+    <UserProgressContextProvider>
+      <CartContextProvider>
       <Header />
-      <main>
-        <OpinionsContextProvider>
-          <NewOpinion />
-          <Opinions />
-        </OpinionsContextProvider>
-      </main>
-    </>
+      <Meals />
+      <Cart />
+      <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
