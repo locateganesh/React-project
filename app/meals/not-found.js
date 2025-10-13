a6d5c7314f659d2a2f172f668dbc02e9da1cbda1
ganyadav <99638611+ganyadav@users.noreply.github.com>
2025-10-13 14:26:18 +0530

    next js pages router

--- app/meals/not-found.js
+++
@@ -1,9 +0,0 @@
-// reserved name.
-// show when url mis-typed.
-// NotFoundPage also a reserved name
-export default function NotFoundPage() {
-    return <main className="not-found">
-        <h1>Meal not Found</h1>
-        <p>Unfortunatly, We cannot found the meal, which you have requested.</p>
-    </main>
-}
--- app/not-found.js
+++
@@ -1,9 +0,0 @@
-// reserved name.
-// show when url mis-typed.
-// NotFoundPage also a reserved name
-export default function NotFoundPage() {
-    return <main className="not-found">
-        <h1>Not Found</h1>
-        <p>Unfortunatly, We cannot found the page, which you have requested.</p>
-    </main>
-}
